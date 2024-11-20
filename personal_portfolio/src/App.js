c517b052a61913140cd5289de7e9919f4fc3e2e6
Dineshkumarkota <143687187+Dineshkumarkota@users.noreply.github.com>
2024-11-20 10:17:52 +0530

    added navbar to App.js

---
+++ personal_portfolio/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+function App(){
+  return(
+    <div>
+    {/*header*/}
+    <header>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href='About me'>About Me</a></li>
+          <li><a href='projects'>Projects</a></li>
+          <li><a href='skills'>Skills</a></li>
+          <li><a href='contact'>Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    </div>
+  )
+}
+export default App;
